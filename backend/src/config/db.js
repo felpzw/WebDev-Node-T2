02cc352b522b55b50ead12535a40efc13c6dbead
felpzw <lipewinsch@gmail.com>
2025-11-15 14:59:21 -0300

    dotenv + db.js with dotent

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 
-const dbUser = "root";
-const dbPass = "root";
-const dbName = "SlideProject";
-
+const { dbUser, dbPass, dbName, dbHost, dbPort} = require("./index");
 
 mongoose
-  .connect(`mongodb://${dbUser}:${dbPass}@localhost:27017/${dbName}?authSource=admin`, {})
+  .connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`, {})
   .then(() => console.log("Conectado ao MongoDB:SlideProject"))
   .catch((err) => console.error("Erro ao conectar:", err));
 
