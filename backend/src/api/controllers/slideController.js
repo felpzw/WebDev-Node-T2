c93c88d45eb6aee3f30b24d8c6f21bccfabc9191
felpzw <lipewinsch@gmail.com>
2025-11-16 13:47:55 -0300

    slideControllet without socket service.

---
+++ backend/src/api/controllers/slideController.js
@@ -0,0 +1,59 @@
+const slideService = require('../services/slideService');
+//possivel socket service aqui - lembrar de testar.
+
+
+const list = async (req, res) => {
+  try {
+    const slides = await slideService.listAllSlides();
+    res.json(slides);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const slide = await slideService.createSlide(req.body);
+    res.status(201).json(slide);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+const update = async (req, res) => {
+  try {
+    const slideId = req.params.id;
+    const slideData = req.body;
+    
+    const slide = await slideService.updateSlide(slideId, slideData);
+    
+    res.json(slide);
+  } catch (error) {
+    if (error.message === 'Slide não encontrado') {
+      return res.status(404).json({ message: error.message });
+    }
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const slideId = req.params.id;
+    await slideService.deleteSlide(slideId);
+    
+    res.status(204).send(); 
+  } catch (error) {
+    if (error.message === 'Slide não encontrado') {
+      return res.status(404).json({ message: error.message });
+    }
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  list,
+  create,
+  update,
+  remove,
+};
